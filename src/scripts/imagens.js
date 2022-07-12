53d4a0873a5b67fa1e8a299e095a8a17e7b93a34
Francisco Carvalho <f.thiene@gmail.com>
2022-07-12 17:12:23 -0300

    Adds more images

--- src/scripts/imagens.js
+++ src/scripts/imagens.js
@@ -1,23 +1,23 @@
 'use strict';
 
 module.exports = [{
-  name: 'clinica-1.jpg',
+  name: 'clinica-1.png',
   alt: 'Recepção da clínica Aurora, vista de frente',
   class: 'width-100-percent'
 }, {
-  name: 'clinica-2.jpg',
+  name: 'clinica-2.png',
   alt: 'Recepção da clínica Aurora, vista de dentro',
   class: 'width-100-percent'
 }, {
-  name: 'clinica-3.jpg',
+  name: 'clinica-3.png',
   alt: 'Recepção da clínica Aurora, vista de dentro',
   class: 'width-100-percent'
 }, {
-  name: 'clinica-4.jpg',
+  name: 'clinica-4.png',
   alt: 'Recepção da clínica Aurora, vista de dentro',
   class: 'width-100-percent'
 }, {
-  name: 'clinica-5.jpg',
+  name: 'clinica-5.png',
   alt: 'Recepção da clínica Aurora, vista de dentro',
   class: 'width-100-percent'
 }, {
@@ -25,11 +25,27 @@ module.exports = [{
   alt: 'Recepção da clínica Aurora, vista de dentro',
   class: 'width-100-percent'
 }, {
-  name: 'clinica-7.jpg',
+  name: 'clinica-7.png',
   alt: 'Recepção da clínica Aurora, vista de dentro',
   class: 'width-100-percent'
 }, {
-  name: 'clinica-8.jpg',
+  name: 'clinica-8.png',
+  alt: 'Recepção da clínica Aurora, vista de dentro',
+  class: 'width-100-percent'
+}, {
+  name: 'clinica-9.png',
+  alt: 'Recepção da clínica Aurora, vista de dentro',
+  class: 'width-100-percent'
+}, {
+  name: 'clinica-10.png',
+  alt: 'Recepção da clínica Aurora, vista de dentro',
+  class: 'width-100-percent'
+}, {
+  name: 'clinica-11.png',
+  alt: 'Recepção da clínica Aurora, vista de dentro',
+  class: 'width-100-percent'
+}, {
+  name: 'clinica-12.png',
   alt: 'Recepção da clínica Aurora, vista de dentro',
   class: 'width-100-percent'
 }];
