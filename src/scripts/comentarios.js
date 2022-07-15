3365dc8340abba7df04b3dee73f437de42c169f5
Francisco Carvalho <f.thiene@gmail.com>
2022-07-14 21:54:41 -0300

    Inserts comments texts

--- src/scripts/comentarios.js
+++ src/scripts/comentarios.js
@@ -1,93 +1,123 @@
 'use strict';
 
 module.exports = [{
+  visible: false,
   png: 'p-thais.png',
   webp: 'p-thais.webp',
   name: 'Thais de Paula',
   text: 'Curabitur id vestibulum velit, vel lobortis odio. Nulla facilisi. Nulla pretium varius velit suscipit malesuada. Sed vel lobortis lorem, at tincidunt nisl. Pellentesque eleifend libero erat, vel vehicula tellus aliquam a.'
 }, {
+  visible: true,
   png: 'p-marina.png',
   webp: 'p-marina.webp',
   name: 'Marina Laport',
-  text: 'Curabitur id vestibulum velit, vel lobortis odio. Nulla facilisi. Nulla pretium varius velit suscipit malesuada. Sed vel lobortis lorem, at tincidunt nisl. Pellentesque eleifend libero erat, vel vehicula tellus aliquam a.'
+  text: 'Excelente atendimento!! Dra Luciana é muito atenciosa e ainda me deu um <a href="/img/kit-marina.jpeg" target="_blank">kit super fofo</a> 😍😍 clínica mais linda e charmosa de Ouro Preto.'
 }, {
+  visible: true,
   png: 'p-paulo.png',
   webp: 'p-paulo.webp',
   name: 'Paulo Silva',
-  text: 'Curabitur id vestibulum velit, vel lobortis odio. Nulla facilisi. Nulla pretium varius velit suscipit malesuada. Sed vel lobortis lorem, at tincidunt nisl. Pellentesque eleifend libero erat, vel vehicula tellus aliquam a.'
+  text: 'Ótimo atendimento e profissionalismo, recomendo demais.'
 }, {
+  visible: true,
   png: 'p-lydiane.png',
   webp: 'p-lydiane.webp',
-  name: 'Lydiane Ribeiro',
-  text: 'Curabitur id vestibulum velit, vel lobortis odio. Nulla facilisi. Nulla pretium varius velit suscipit malesuada. Sed vel lobortis lorem, at tincidunt nisl. Pellentesque eleifend libero erat, vel vehicula tellus aliquam a.'
+  name: 'Lidyane Ribeiro',
+  text: 'Excelente profissional! Super atenciosa e dedicada em tudo que fala e faz! Ja estou ansiosa para a próxima consulta!'
 }, {
+  visible: true,
   png: 'p-ana.png',
   webp: 'p-ana.webp',
   name: 'Ana Carolina',
-  text: 'Curabitur id vestibulum velit, vel lobortis odio. Nulla facilisi. Nulla pretium varius velit suscipit malesuada. Sed vel lobortis lorem, at tincidunt nisl. Pellentesque eleifend libero erat, vel vehicula tellus aliquam a.'
+  text: 'Dra. Luciana é maravilhosa… Trabalho impecável, amei e super indico! ❤️'
 }, {
+  visible: true,
   png: 'p-fonti.png',
   webp: 'p-fonti.webp',
   name: 'Eduardo Fontinelle',
-  text: 'Curabitur id vestibulum velit, vel lobortis odio. Nulla facilisi. Nulla pretium varius velit suscipit malesuada. Sed vel lobortis lorem, at tincidunt nisl. Pellentesque eleifend libero erat, vel vehicula tellus aliquam a.'
+  text: 'Fui muito bem atendido! A Dra Luciana foi muito atenciosa. Gostei tanto que levei minha filha também!'
 }, {
+  visible: true,
   png: 'p-luqueta.png',
   webp: 'p-luqueta.webp',
   name: 'Lucas Silva',
-  text: 'Curabitur id vestibulum velit, vel lobortis odio. Nulla facilisi. Nulla pretium varius velit suscipit malesuada. Sed vel lobortis lorem, at tincidunt nisl. Pellentesque eleifend libero erat, vel vehicula tellus aliquam a.'
+  text: 'Atendimento excepcional e ambiente muito agradável. Melhor clínica da região.'
 }, {
+  visible: true,
   png: 'p-chupis.png',
   webp: 'p-chupis.webp',
   name: 'Filipe Santos',
-  text: 'Curabitur id vestibulum velit, vel lobortis odio. Nulla facilisi. Nulla pretium varius velit suscipit malesuada. Sed vel lobortis lorem, at tincidunt nisl. Pellentesque eleifend libero erat, vel vehicula tellus aliquam a.'
+  text: 'A melhor clínica odontológica de Ouro Preto. Excelente atendimento!'
 }, {
+  visible: true,
   png: 'p-amanda.png',
   webp: 'p-amanda.webp',
   name: 'Amanda Ovcearenco',
-  text: 'Curabitur id vestibulum velit, vel lobortis odio. Nulla facilisi. Nulla pretium varius velit suscipit malesuada. Sed vel lobortis lorem, at tincidunt nisl. Pellentesque eleifend libero erat, vel vehicula tellus aliquam a.'
+  text: 'Ambiente asseado, bonito e atendimento impecável'
 }, {
+  visible: true,
   png: 'p-caetano.png',
   webp: 'p-caetano.webp',
   name: 'Caetano Carvalho',
-  text: 'Curabitur id vestibulum velit, vel lobortis odio. Nulla facilisi. Nulla pretium varius velit suscipit malesuada. Sed vel lobortis lorem, at tincidunt nisl. Pellentesque eleifend libero erat, vel vehicula tellus aliquam a.'
+  text: 'A Aurora odontologia é realmente diferenciada na prevenção e no tratamento da saúde oral. Competência e confiabilidade dos profissionais. Recebi uma orientação completa sobre  higiene bucal bem como o material utilizado na escovação dos dentes. Eu recomendo.'
 }, {
+  visible: true,
   png: 'p-beth.png',
   webp: 'p-beth.webp',
   name: 'Beth Carvalho',
-  text: 'Curabitur id vestibulum velit, vel lobortis odio. Nulla facilisi. Nulla pretium varius velit suscipit malesuada. Sed vel lobortis lorem, at tincidunt nisl. Pellentesque eleifend libero erat, vel vehicula tellus aliquam a.'
+  text: 'Me senti em casa, segura de ter feito uma ótima escolha em relação a minha saúde bucal. Acolhimento e trabalho de qualidade de primeira. Recomendo a todos!!'
 }, {
+  visible: true,
   png: 'p-gabriel.png',
   webp: 'p-gabriel.webp',
   name: 'Gabriel José',
-  text: 'Curabitur id vestibulum velit, vel lobortis odio. Nulla facilisi. Nulla pretium varius velit suscipit malesuada. Sed vel lobortis lorem, at tincidunt nisl. Pellentesque eleifend libero erat, vel vehicula tellus aliquam a.'
+  text: 'Atendimento completo e de qualidade. Ambiente e infraestrutura da clínica são completamente diferenciados.'
 }, {
+  visible: true,
   png: 'p-maria.png',
   webp: 'p-maria.webp',
   name: 'Maria Silveira',
-  text: 'Curabitur id vestibulum velit, vel lobortis odio. Nulla facilisi. Nulla pretium varius velit suscipit malesuada. Sed vel lobortis lorem, at tincidunt nisl. Pellentesque eleifend libero erat, vel vehicula tellus aliquam a.'
+  text: 'Clínica bem estruturada, atendimento humanizado, a melhor da região!  Fui muito bem recepcionada na clínica e recebi um atendimento maravilhoso!'
 }, {
+  visible: true,
   png: 'p-joao.png',
   webp: 'p-joao.webp',
   name: 'João Pedro',
-  text: 'Curabitur id vestibulum velit, vel lobortis odio. Nulla facilisi. Nulla pretium varius velit suscipit malesuada. Sed vel lobortis lorem, at tincidunt nisl. Pellentesque eleifend libero erat, vel vehicula tellus aliquam a.'
+  text: 'Excelente atendimento, equipe muito atenciosa e qualificada. Valores muito justos. Bastante satisfeito com o serviço!'
 }, {
+  visible: true,
   png: 'p-genilson.png',
   webp: 'p-genilson.webp',
   name: 'Genilson Israel',
-  text: 'Curabitur id vestibulum velit, vel lobortis odio. Nulla facilisi. Nulla pretium varius velit suscipit malesuada. Sed vel lobortis lorem, at tincidunt nisl. Pellentesque eleifend libero erat, vel vehicula tellus aliquam a.'
+  text: 'Profissionalismo e eficiência, recomendo muito!'
 }, {
+  visible: true,
   png: 'p-tairine.png',
   webp: 'p-tairine.webp',
   name: 'Tairine Bertola',
-  text: 'Curabitur id vestibulum velit, vel lobortis odio. Nulla facilisi. Nulla pretium varius velit suscipit malesuada. Sed vel lobortis lorem, at tincidunt nisl. Pellentesque eleifend libero erat, vel vehicula tellus aliquam a.'
+  text: 'Atendimento nota mil!'
 }, {
+  visible: false,
   png: 'p-tulio.png',
   webp: 'p-tulio.webp',
   name: 'Tulio Spuri',
   text: 'Curabitur id vestibulum velit, vel lobortis odio. Nulla facilisi. Nulla pretium varius velit suscipit malesuada. Sed vel lobortis lorem, at tincidunt nisl. Pellentesque eleifend libero erat, vel vehicula tellus aliquam a.'
 }, {
+  visible: false,
   png: 'p-cecilia.png',
   webp: 'p-cecilia.webp',
   name: 'Cecília Devêza',
   text: 'Curabitur id vestibulum velit, vel lobortis odio. Nulla facilisi. Nulla pretium varius velit suscipit malesuada. Sed vel lobortis lorem, at tincidunt nisl. Pellentesque eleifend libero erat, vel vehicula tellus aliquam a.'
+}, {
+  visible: true,
+  png: 'p-adriana.png',
+  webp: 'p-adriana.webp',
+  name: 'Adriana Ovcearenco',
+  text: 'Clínica muito boa, equipe acolhedora, profissionais maravilhosos, atendem com muito carinho e dedicação. Amei e indico pra todos!'
+}, {
+  visible: true,
+  png: 'p-anaflavia.png',
+  webp: 'p-anaflavia.webp',
+  name: 'Ana Flávia',
+  text: 'Equipe muito profissional. Fui muito bem atendida nos meus tratamentos. Indico a clínica para quem procura um tratamento profissional e exigente.'
 }];
